e5a5d0919d155782c9d636508b5b0b83bbabcc8d
Alex Togobytskyy <ImAlexTogo@gmail.com>
2019-10-30 05:00:25 -0400

    added splash screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuestionsComponent } from './questions/questions.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material';
 
 @NgModule({
     declarations: [AppComponent, QuestionsComponent],
-    imports: [BrowserModule, AppRoutingModule, FlexLayoutModule, MatIconModule, BrowserAnimationsModule ],
+    imports: [BrowserModule, AppRoutingModule, FlexLayoutModule, BrowserAnimationsModule, MatButtonModule],
     providers: [],
     bootstrap: [AppComponent]
 })
