72baf88a4136fb67fbcc4cc1139fff4f4c9ad259
Alex Togobytskyy <ImAlexTogo@gmail.com>
2019-10-30 04:17:18 -0400

    added material/animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuestionsComponent } from './questions/questions.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [AppComponent, QuestionsComponent],
-    imports: [BrowserModule, AppRoutingModule, FlexLayoutModule],
+    imports: [BrowserModule, AppRoutingModule, FlexLayoutModule, MatIconModule, BrowserAnimationsModule ],
     providers: [],
     bootstrap: [AppComponent]
 })
